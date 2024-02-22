8a1c77610288f0e82b4ace973bb90ff61c1aa3c5
Caio César <caiogalaxy00@gmail.com>
2024-02-22 09:48:11 -0300

    Aula1

---
+++ Fundamentos_de_Software/Aula1.C
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+// Aspectos técnicos do software
+
+/*
+obter notas
+calcular media
+verificar se o aluno foi reprovado ou não
+se a media >= 7 aprovado
+se não reprovado
+*/
+
+int main(void)
+{
+    // decraração de variaveis
+    float nota1, nota2, nota3, media;
+
+    // obter as notas
+    printf("Digite a primeira nota: ");
+    scanf("%f", &nota1);
+
+    printf("Digite a segunda nota: ");
+    scanf("%f", &nota2);
+
+    // calcular a media
+    media = (nota1 + nota2) / 2;
+
+    // veriicar se foi aprovado ou não
+    if (media >= 7)
+        printf("Aprovado");
+    else
+        printf("Reprovado");
+
+    return 0;
+}
